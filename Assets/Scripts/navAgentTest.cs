cd253678db5ab60d4597f6cb78650028d1a85455
dartagnan sams <dartagnansams1@icloud.com>
2024-09-30 07:36:40 -0400

    started on design for gameplay map

--- Assets/Scripts/navAgentTest.cs
+++ Assets/Scripts/navAgentTest.cs
@@ -7,16 +7,31 @@ public class navAgentTest : MonoBehaviour
 {
     public GameObject navTarget;
     NavMeshAgent agent;
+    Animator anim;
     // Start is called before the first frame update
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(navTarget.transform.position);
+        //Debug.Log((Vector3.Distance(transform.position, navTarget.transform.position)));
+        if (Vector3.Distance(transform.position,navTarget.transform.position) > 2.2f)
+        {
+            anim.SetBool("chase", true);
+            agent.SetDestination(navTarget.transform.position);
+        } 
+
+        if (Vector3.Distance(transform.position, navTarget.transform.position) < 2)
+        {
+            anim.SetBool("chase", false);
+            anim.SetTrigger("attack");
+        }
+
+
     }
 
     private void OnTriggerEnter(Collider other)
